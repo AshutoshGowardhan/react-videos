964c303f73c556c25df77c25a0d55c6bd62fe00f
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-05-26 23:23:20 +0530

    youtube api and axios

---
+++ src/apis/Youtube.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+
+const KEY = 'AIzaSyCvqMdfu7cwCr60RmHOGbPGkcXOuyk6rY8'
+
+export default axios.create({
+    baseURL: 'GET https://www.googleapis.com/youtube/v3',
+    params: {
+        part: snippet,
+        maxResults: 5,
+        type: 'video',
+        key : KEY,
+    }
+
+})
